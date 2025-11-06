7cadd64d9ec3451499b7059232ea1e6222141840
damihvi <dami-anherrera@hotmail.com>
2025-11-05 19:51:28 -0500

    clase 05/11/2025

--- modulo_2/react_native/rn-purple-app/src/services/auth.service.ts
+++ modulo_2/react_native/rn-purple-app/src/services/auth.service.ts
@@ -1,20 +1,26 @@
 import api from "./api";
 import { authStore } from "../store/auth";
+import { Platform } from "react-native";
 
 // API pública: https://reqres.in
-const BASE = "https://reqres.in/api";
+const host = Platform.OS === "android" ? "10.0.2.2" : "localhost";
+const BASE = `http://${host}:8000/api/auth`;
 
 export async function login(email: string, password: string) {
-  const { data } = await api.post(`${BASE}/login`, { email, password });
+  const { data } = await api.post(`${BASE}/login`, {username:email, password });
   authStore.set({ token: data.token, email });
   return data;
 }
 
 export async function register(email: string, password: string) {
-  const { data } = await api.post(`${BASE}/register`, { email, password });
-  // Opcional: guardar token si devuelve
-  authStore.set({ token: data.token ?? null, email });
-  return data;
+  try {
+    const { data } = await api.post(`${BASE}/register`, {username:email, email, password });
+    authStore.set({ token: data.token, email });
+    return data;
+  } catch (err: any) {
+    console.log("register error:", err.response?.data || err.message);
+    throw err;
+  }
 }
 
 export function logout() {
